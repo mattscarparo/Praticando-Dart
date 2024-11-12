f31d8d994f164efb3e8230c9deb570902c84dadf
Matheus Scarparo <mat.scarparo@gmail.com>
2024-11-12 15:15:03 -0300

    Update calculadora.dart
    Refatorando e adicionando mais funcionalidades para o usuário

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 void main() {
-  double primeiroNumero = double.parse(stdin.readLineSync()!);
-  double segundoNumero = double.parse(stdin.readLineSync()!);
+  double primeiroNumero = 0;
+  double segundoNumero = 0;
+  String operacao = "";
+  String? entrada = "";
+  List<String> operacoes = <String>["+", "-", "*", "/"];
 
   void soma() {
     print(primeiroNumero + segundoNumero);
@@ -20,5 +23,55 @@ void main() {
     print(primeiroNumero / segundoNumero);
   }
 
-  soma();
+  void calcular() {
+    switch (operacao) {
+      case "+":
+        soma();
+
+      case "-":
+        subtracao();
+
+      case "*":
+        multiplicacao();
+
+      case "/":
+        divisao();
+        break;
+    }
+  }
+
+  void getOperacao() {
+    print("Digite uma operação: ${operacoes.toString()}");
+    entrada = stdin.readLineSync();
+    if (entrada != null) {
+      if (operacoes.contains(entrada)) {
+        operacao = entrada!;
+      } else {
+        print("Operação inválida!");
+        getOperacao();
+      }
+    }
+  }
+
+  print("Digite o primeiro valor: ");
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    if (entrada != "") {
+      primeiroNumero = double.parse(entrada!);
+    }
+  }
+
+  getOperacao();
+
+  print("Digite o segundo valor: ");
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    if (entrada != "") {
+      segundoNumero = double.parse(entrada!);
+    }
+  }
+
+  print("O resultado da operação é: ");
+
+  calcular();
 }
